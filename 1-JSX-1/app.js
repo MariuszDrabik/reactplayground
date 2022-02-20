3395efec282c9e27fae652104e7cc12586d13d82
Mariusz Dell <drabik.mariusz@gmail.com>
2022-02-20 20:21:35 +0100

    3

--- 1-JSX-1/app.js
+++ 1-JSX-1/app.js
@@ -5,7 +5,7 @@ const classBig = 'big'
 const main =(
     <div>
         <h1 onClick={ () => {alert("KLICK")}} className="red">Pierwszy artykuł</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi laboriosam odit perferendis quae sequi accusamus facere. Tempore eius necessitatibus fuga, minima, corrupti est commodi quod voluptatem obcaecati quidem iste eos?</p>
+        <p>Loremd ipsum dolor sit amet consectetur adipisicing elit. Eligendi laboriosam odit perferendis quae sequi accusamus facere. Tempore eius necessitatibus fuga, minima, corrupti est commodi quod voluptatem obcaecati quidem iste eos?</p>
     </div>
 )
 
