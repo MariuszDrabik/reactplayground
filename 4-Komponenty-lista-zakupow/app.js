3487cee2fbfdd0ac27038e0bda44cd8ab9db1b3d
Mariusz Drabik <drabik.mariusz@gmail.com>
2022-04-18 20:53:43 +0200

    testy

--- 4-Komponenty-lista-zakupow/app.js
+++ 4-Komponenty-lista-zakupow/app.js
@@ -1,11 +1,28 @@
 class ShoppingList extends React.Component {
-    state {
+    state = {
+        item_1: 'Marchew',
+        item_2: 'Ogórek',
+        item_3: 'Pomidor',
         
     }
 
     render(){
         return (
+            <>
+                <h1>Lista Zakupów</h1>
+                <ul>
+                    <itemList />
+                    <itemList />
+                    <itemList />
+                </ul>
+            </>
 
         )
     }
 }
+
+const itemList = () => {
+    
+}
+
+ReactDOM.render(<ShoppingList />, document.getElementById('root'))
